5a75611c4d942c11afc01d4a7bdfa9cd87e09ab7
cnotv <contact@cnotv.xyz>
2018-02-25 12:12:46 +0100

    syntax fix

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -11,7 +11,7 @@ import { Recipe } from '../recipe.model';
 export class RecipeListComponent implements OnInit {
   @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
-    new Recipe('Summer Roll', 'Healthy food from Asia', 'https://atmedia.imgix.net/71f67d97b5c7acf9989296ec68ea22a5cc6bb79c?auto=format&q=45&w=1033.0&fit=max&cs=strip')
+    new Recipe('Summer Roll', 'Healthy food from Asia', 'https://atmedia.imgix.net/71f67d97b5c7acf9989296ec68ea22a5cc6bb79c?auto=format&q=45&w=1033.0&fit=max&cs=strip'),
     new Recipe('Spring Roll', 'Healthy food from Asia', 'https://atmedia.imgix.net/71f67d97b5c7acf9989296ec68ea22a5cc6bb79c?auto=format&q=45&w=1033.0&fit=max&cs=strip')
   ];
 
