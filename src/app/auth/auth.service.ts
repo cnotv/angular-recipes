ffe2821570c4d7d928d8aef2e7a64f0bdc3ea0dc
cnotv <contact@cnotv.xyz>
2018-03-07 11:00:23 +0100

    added Auth Guards and conditionals for navigation

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,11 +1,15 @@
-import * as firebase from 'firebase';
-// import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { Injectable } from '@angular/core';
 
-// @Injectable()
+import * as firebase from 'firebase';
+
+@Injectable()
 export class AuthService {
   token: string;
 
-  constructor() { }
+  constructor(
+    private router: Router
+  ) { }
 
   signupUser(email: string, password: string) {
     firebase.auth().createUserWithEmailAndPassword(email, password)
@@ -18,6 +22,7 @@ export class AuthService {
     firebase.auth().signInWithEmailAndPassword(email, password)
       .then(
         response => {
+          this.router.navigate(['/'])
           firebase.auth().currentUser.getIdToken()
             .then(
               (token: string) => this.token = token
